4c1fe7d0e85b04b15651e0fb39bbf96e5b96be96
VictorySpecificationII <victoryspecificationii@gmail.com>
2016-09-17 18:52:07 +0100

    added dialogue for in car sessions and tweaked python file

--- AIML/startup.py
+++ AIML/startup.py
@@ -16,15 +16,16 @@ sessionData = kernel.getSessionData(sessionId)
 # The predicate name is the name of something/someone
 # that the bot knows about in your session with the bot
 # The bot might know you as "Billy" and that your "dog" is named "Brandy"
-kernel.setPredicate("dog", "Brandy", sessionId)
-clients_dogs_name = kernel.getPredicate("dog", sessionId)
+#kernel.setPredicate("dog", "Brandy", sessionId)
+#clients_dogs_name = kernel.getPredicate("dog", sessionId)
 
-kernel.setBotPredicate("hometown", "127.0.0.1")
-bot_hometown = kernel.getBotPredicate("hometown")
+#kernel.setBotPredicate("hometown", "127.0.0.1")
+#bot_hometown = kernel.getBotPredicate("hometown")
 
 if os.path.isfile("aidens_brain.brn"):
     kernel.bootstrap(brainFile = "aidens_brain.brn")
 else:
+    kernel.bootstrap(learnFiles = "std-startup.xml", commands = "load aiml a")
     kernel.bootstrap(learnFiles = "std-startup.xml", commands = "load aiml b")
     kernel.saveBrain("aidens_brain.brn")
 
@@ -32,7 +33,7 @@ else:
 while True:
 	
     message = raw_input(">> ")
-    print kernel.respond(message)
+    
     if message == "quit":
         exit()
     elif message == "save":
@@ -41,3 +42,4 @@ while True:
     	os.system("clear")
     else:
         bot_response = kernel.respond(message)
+        print bot_response
